bf5326eb2ef1a53b6df8405d4d5cc196892fd6aa
RikkiRun <31293953+RikkiRun@users.noreply.github.com>
2018-12-23 18:37:52 -0700

    chapter 7 Diffuse Materials

--- main.cpp
+++ main.cpp
@@ -10,10 +10,21 @@
 using namespace std;
 double drand48(void);
 
+vec3 random_in_unit_sphere() {
+	vec3 p;
+	do {
+		p = 2.0*vec3(drand48(), drand48(), drand48()) - vec3(1, 1, 1);
+
+	} while (p.squared_length() >= 1.0); {
+		return p;
+	}
+}
+
 vec3 color(const ray& r, hitable *world) {
 	hit_record rec;
 	if (world->hit(r, 0.0, FLT_MAX, rec)) {
-		return 0.5*vec3(rec.normal.x() + 1, rec.normal.y() + 1, rec.normal.z() + 1);
+		vec3 target = rec.p + rec.normal + random_in_unit_sphere();
+		return 0.5 * color(ray(rec.p, target - rec.p), world);
 	}
 	else {
 		vec3 unit_direction = unit_vector(r.direction());
@@ -29,7 +40,7 @@ int main()
 	int ny = 100;
 	int ns = 100;
 
-	ofstream outfile("chapter5.ppm", ios_base::out);
+	ofstream outfile("chapter7.ppm", ios_base::out);
 	// Output to .ppm file
 	outfile << "P3\n" << nx << " " << ny << "\n255\n";
 	// output to command line
