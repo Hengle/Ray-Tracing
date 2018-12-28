455287d0ddf26cf5e98287814e8012b293ffcd0e
RikkiRun <31293953+RikkiRun@users.noreply.github.com>
2018-12-28 03:16:44 -0700

    chapter 12 - final render

--- main.cpp
+++ main.cpp
@@ -11,6 +11,35 @@
 using namespace std;
 double drand48(void);
 
+hitable *random_scene() {
+	int n = 500;
+	hitable **list = new hitable *[n + 1];
+	list[0] = new sphere(vec3(0, -1000, 0), 1000, new lambertian(vec3(0.5, 0.5, 0.5)));
+	int i = 1;
+	for (int a = -11; a < 11; a++) {
+		for (int b = -11; b < 11; b++) {
+			float choose_mat = drand48();
+			vec3 center(a + 0.9*drand48(), 0.2, b + 0.9*drand48());
+			if ((center - vec3(4, 0.2, 0)).length() > 0.9) {
+				if (choose_mat < 0.8) {
+					list[i++] = new sphere(center, 0.2, new lambertian(vec3(drand48()*drand48(), drand48()*drand48(), drand48()*drand48())));
+				}
+				else if(choose_mat<0.95){
+					list[i++] = new sphere(center, 0.2, new metal(vec3(0.5*(1+drand48()), 0.5*(1 + drand48()), 0.5*(1 + drand48())), 0.5*drand48()));
+				}
+				else {
+					list[i++] = new sphere(center, 0.2, new dielectric(1.5));
+				}
+			}
+		}
+	}
+	list[i++] = new sphere(vec3(0, 1, 0), 1.0, new dielectric(1.5));
+	list[i++] = new sphere(vec3(-4, 1, 0), 1.0, new lambertian(vec3(0.4, 0.2, 0.1)));
+	list[i++] = new sphere(vec3(4, 1, 0), 1.0, new metal(vec3(0.7, 0.6, 0.5), 0.0));
+
+	return new hitable_list(list, i);
+}
+
 vec3 color(const ray& r, hitable *world, int depth) {
 	hit_record rec;
 	if (world->hit(r, 0.001, FLT_MAX, rec)) {
@@ -34,11 +63,11 @@ vec3 color(const ray& r, hitable *world, int depth) {
 int main()
 {
 
-	int nx = 200;
-	int ny = 100;
-	int ns = 100;
+	int nx = 1200;
+	int ny = 800;
+	int ns = 10;
 
-	ofstream outfile("chapter11.ppm", ios_base::out);
+	ofstream outfile("chapter13.ppm", ios_base::out);
 	// Output to .ppm file
 	outfile << "P3\n" << nx << " " << ny << "\n255\n";
 	// output to command line
@@ -53,11 +82,12 @@ int main()
 
 
 	hitable *world = new hitable_list(list, 5);
+	world = random_scene();
 
-	vec3 lookfrom(3, 3, 2);
-	vec3 lookat(0, 0, -1);
-	float dist_to_focus = (lookfrom - lookat).length();
-	float aperture = 2.0;
+	vec3 lookfrom(13, 2, 3);
+	vec3 lookat(0, 0, 0);
+	float dist_to_focus = 10;
+	float aperture = 0.1;
 	camera cam(lookfrom, lookat,vec3(0, 1, 0), 30, float(nx) / float(ny), aperture, dist_to_focus);
 	// Draw image pixels from top to bottom, left to right
 	for (int j = ny-1; j >= 0; j--)
